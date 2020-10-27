22ce13b876a7b94e1124a065f7fdb9988eff396a
Franccesco Antonio <franccesco_@hotmail.com>
2020-10-27 09:39:07 -0300

    native, host & user

---
+++ JavaScript Completo ES6/3-objetos/3-native-host-user/index.js
@@ -0,0 +1,31 @@
+// console.log(typeof Array.fromasdasfs !== undefined);
+
+// if (typeof Array.fromasdasfs !== 'undefined') {
+//   console.log('Existe');
+// } else {
+//   console.log('Não Existe');
+// }
+
+// Exercicios
+// Liste 5 objetos nativos
+Object;
+String;
+Array;
+Function;
+Number;
+
+// Liste 5 objetos do browser
+Window;
+NodeList;
+history;
+Document;
+HTMLCollection;
+
+// Liste 2 Métodos, Propriedades ou Objetos
+// presentes no Chrome que não existem no Firefox
+
+if (typeof window.webkitMediaStream !== 'undefined') {
+  console.log('Existe');
+} else {
+  console.log('Não existe');
+}
